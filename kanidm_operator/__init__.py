ec43f141adf086174280acdd4990c879d89800a6
Marcus Bannerman <m.bannerman@gmail.com>
2024-05-26 22:29:47 +0100

    removed accounts for now

--- kanidm_operator/__init__.py
+++ kanidm_operator/__init__.py
@@ -2,11 +2,6 @@
 # pylint: disable=unused-import
 
 from .deploy.kanidm import on_create_kanidms, on_update_kanidms  # noqa
-from .deploy.account import (
-    on_create_account,
-    on_update_account,
-    on_delete_account,
-)  # noqa
 from .deploy.group import (
     on_create_group,
     on_update_group_name,
