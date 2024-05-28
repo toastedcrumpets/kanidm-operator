686f583930b411ce56538ef5d02768e0149bfe13
Marcus Bannerman <m.bannerman@gmail.com>
2024-05-28 13:26:39 +0100

    More work on testing the API

--- tests/test_kanidm_operator.py
+++ tests/test_kanidm_operator.py
@@ -3,6 +3,7 @@ import subprocess
 import os.path
 import kopf.testing
 import pytest
+import yaml
 
 crd_yaml = os.path.relpath(os.path.join(os.path.dirname(__file__), '..', 'manifests/crds'))
 obj_yaml = os.path.relpath(os.path.join(os.path.dirname(__file__), '..', 'manifests/operator'))
@@ -20,22 +21,31 @@ def test_resource_lifecycle():
     settings = kopf.OperatorSettings()
     settings.watching.server_timeout = 10
 
-    # Create a k8s client
-    from kubernetes import client, config, utils
+    # Create a k8s client for normal operations
+    from kubernetes import client, config, utils, dynamic
+    from kubernetes.client import api_client
     config.load_kube_config()
     k8s_client = client.ApiClient()
     namespace = utils.create_from_yaml(k8s_client, os.path.join(example, 'namespace.yaml'))
     print("Created namespace", namespace)
+
+    # We need a client just for our CRDs
+    client = dynamic.DynamicClient(
+        api_client.ApiClient(configuration=config.load_kube_config())
+    )
+    # fetching the custom resource definition (CRD) api
+    crd_api = client.resources.get(
+        api_version="kanidm.github.io/v1alpha1", kind="CustomResourceDefinition"
+    )
 
     # Run the operator and simulate some activity!
     with kopf.testing.KopfRunner(
         ['run', '--all-namespaces', '--standalone', "-m", "kanidm_operator"], #"--verbose",
         timeout=60, settings=settings,
     ) as runner:
-
-        Kanidm = utils.create_from_yaml(k8s_client, os.path.join(example, 'kanidm.yaml'))
-        print("Kanidm deployment", Kanidm, repr(Kanidm), type(Kanidm))
-        time.sleep(1)  # give it some time to react
+        kanidm_yaml = yaml.safe_load(os.path.join(example, 'kanidm.yaml'))
+        kanidm = crd_api.create(kanidm_yaml)
+        print("Kanidm deployment", kanidm, repr(kanidm), type(kanidm))
 
 
     # Ensure that the operator did not die on start, or during the operation.
